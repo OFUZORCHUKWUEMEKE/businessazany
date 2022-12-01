12e79680bbb1dbcc660d2cf95e9fa23fe0357270
OFUZOR <uniben2018@gmail.com>
2022-12-01 13:15:07 +0100

    previous commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import Landing from './pages/Event/Landing';
 import CreateEvent from './pages/Event/CreateEvent';
 import SalesHistory from './pages/Movies/SalesHistory';
 import EditMovie from './pages/Movies/EditMovie';
-import CreateEventTicket from './pages/Event/CreateTicket';
 import CreateTicketEvent from './pages/Event/CreateTicket';
 import EditEvent from './pages/Event/EditEvent';
 import Edit2 from './pages/Event/Edit2';
@@ -36,21 +35,22 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path='/businessVerification' element={<BusinessVerification />} />
       <Route path='/emailVerification' element={<EmailVerification />} />
+
       {/* movie route */}
       <Route path="/movie/landing" element={<Event />} />
-      <Route path="/movie/create" element={<CreateTicket/>} />
+      <Route path="/movie/create" element={<CreateTicket />} />
       <Route path="/movie/edit" element={<Edit />} />
       <Route path="/movie/ticket" element={<Ticket />} />
-      <Route path="/movie/salesHistory" element={<SalesHistory/>} />
-      <Route path="/movie/editmovie" element={<EditMovie/>} />
-      <Route path="/movie/createticket" element={<EditMovie/>} />
+      <Route path="/movie/salesHistory" element={<SalesHistory />} />
+      <Route path="/movie/editmovie" element={<EditMovie />} />
+      <Route path="/movie/createticket" element={<EditMovie />} />
 
       {/* event route */}
-      <Route path="/event/landing" element={<Landing/>}/>
-      <Route path="/event/create" element={<CreateEvent/>}/>
-      <Route path="/event/edit" element={<Edit2/>}/>
-      <Route path="/event/createEventTicket" element={<CreateTicketEvent/>}/>
-      <Route path="/event/editEvent" element={<EditEvent/>}/>
+      <Route path="/event/landing" element={<Landing />} />
+      <Route path="/event/create" element={<CreateEvent />} />
+      <Route path="/event/edit" element={<Edit2 />} />
+      <Route path="/event/createEventTicket" element={<CreateTicketEvent />} />
+      <Route path="/event/editEvent" element={<EditEvent />} />
     </Routes>
   );
 }
